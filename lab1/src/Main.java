7e0804f4233f71799cab8e5997224203b2670d07
bundamm <tomcio.szewa@gmail.com>
2023-10-09 10:35:03 +0200

    oh

---
+++ lab1/src/Main.java
@@ -0,0 +1,10 @@
+
+// A - Operacje wejścia wyjścia
+// B - Instr. warunkowe
+// C - Pętle
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+    }
+}
