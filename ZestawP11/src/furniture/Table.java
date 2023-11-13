fb253af5a4692b77dd8760e7936d4c9c9ff657ed
bundamm <tomcio.szewa@gmail.com>
2023-11-13 08:16:58 +0100

    poprawka

--- ZestawP11/src/furniture/Table.java
+++ ZestawP11/src/furniture/Table.java
@@ -2,9 +2,16 @@ package furniture;
 
 public class Table {
     private double price;
-
+    private static Table instance;
     private Table(double price) {
         this.price = price;
+    }
+
+    public static Table getInstance(double price) {
+        if(instance == null){
+            instance = new Table(price);
+        }
+        return instance;
     }
 
     public double getPrice() {
--- ZestawP11/src/furniture/TestTable.java
+++ ZestawP11/src/furniture/TestTable.java
@@ -2,7 +2,7 @@ package furniture;
 
 public class TestTable {
     public static void main(String[] args) {
-        Table t1 = new Table(123.45);
+        Table t1 = Table.getInstance(123.45);
         System.out.println(t1.getPrice());
         // ale to nie działa bo konstruktor jest prywatny
     }
