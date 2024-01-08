78b4f37028c2ea14ae78f570dce3271ca60600e9
bundamm <tomcio.szewa@gmail.com>
2024-01-08 10:20:00 +0100

    .......

--- lab12/src/zad24/TestTriple.java
+++ lab12/src/zad24/TestTriple.java
@@ -2,7 +2,12 @@ package zad24;
 
 public class TestTriple {
     public static void main(String[] args) {
-
+        Triple<Bird> t = new Triple<>(new Bird("Eagle"), new Bird("Eagl"), new Bird("Penguin"));
+        Bird minBird = findMin(t);
+        System.out.println("minimalny gatunek ptaka: "+minBird.getSpecies());
+        Triple<Eagle> t2 = new Triple<>(new Eagle("egeg",2131), new Eagle("asdac",213134), new Eagle("awfacsdsa",12312));
+        Eagle minEagle = findMin(t2);
+        System.out.println("Minimalny gatunek orła: " +minEagle.getSpecies());
     }
     public static <T extends Bird> T findMin(Triple<? extends T> t){
         T first = t.getOne();
